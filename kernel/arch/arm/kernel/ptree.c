f58365135de4aab014a7a70de6798afadee976c2
W4118 Student <wd2248@columbia.edu>
2014-09-24 17:54:47 -0400

    New version of ptree

--- kernel/arch/arm/kernel/ptree.c
+++ kernel/arch/arm/kernel/ptree.c
@@ -4,7 +4,8 @@
 
 asmlinkage long sys_ptree(struct prinfo *buf, int *nr)
 {
-	int i = 0;
+	read_lock(&tasklist_lock);
+	long i = 0;
 	struct task_struct *p = &init_task;
 	struct task_struct *par = NULL;
 
@@ -14,20 +15,23 @@ asmlinkage long sys_ptree(struct prinfo *buf, int *nr)
 		if (p == par) {
 			i ++;
 			par = p->parent;
-			p = list_entry(sib, struct task_struct, sibling);
+			p = list_entry(sib->next, struct task_struct, sibling);
 			ch = &p->children;
 			sib = &p->sibling;
 		} else if (ch->next == ch) {
 			i ++;
-			p = list_entry(sib, struct task_struct, sibling);
+			p = list_entry(sib->next, struct task_struct, sibling);
 			ch = &p->children;
 			sib = &p->sibling;
 		} else if (ch->next != ch) {
+			i ++;
 			par = p;
-			p = list_entry(ch, struct task_struct, children);
+			p = list_entry(ch->next, struct task_struct, children);
 			ch = &p->children;
 			sib = &p->sibling;
-		}
+		} else
+			return -1;
 	} while (p != &init_task);
+	read_unlock(&tasklist_lock);
 	return i;
 }
