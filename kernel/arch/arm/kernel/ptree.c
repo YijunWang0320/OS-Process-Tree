a78a4ef72684aae7ee292fad6050bf9788b28bdc
Wei Dai <wdai2248@columbia.edu>
2014-09-26 13:32:32 -0400

    used for_each to traverse the tree

--- kernel/arch/arm/kernel/ptree.c
+++ kernel/arch/arm/kernel/ptree.c
@@ -1,37 +1,18 @@
 #include <linux/syscalls.h>
 #include <linux/kernel.h>
+#include <linux/sched.h>
 #include <linux/init_task.h>
 
 asmlinkage long sys_ptree(struct prinfo *buf, int *nr)
 {
-	read_lock(&tasklist_lock);
 	long i = 0;
-	struct task_struct *p = &init_task;
-	struct task_struct *par = NULL;
-
-	struct list_head *ch = &p->children;
-	struct list_head *sib = &p->sibling;
-	do {
-		if (p == par) {
-			i ++;
-			par = p->parent;
-			p = list_entry(sib->next, struct task_struct, sibling);
-			ch = &p->children;
-			sib = &p->sibling;
-		} else if (ch->next == ch) {
-			i ++;
-			p = list_entry(sib->next, struct task_struct, sibling);
-			ch = &p->children;
-			sib = &p->sibling;
-		} else if (ch->next != ch) {
-			i ++;
-			par = p;
-			p = list_entry(ch->next, struct task_struct, children);
-			ch = &p->children;
-			sib = &p->sibling;
-		} else
-			return -1;
-	} while (p != &init_task);
-	read_unlock(&tasklist_lock);
+	struct task_struct *task;
+	
+	for_each_process(task)
+	{
+		i++;
+		printk("%s [%d]\n",task->comm , task->pid);
+	}
+	
 	return i;
 }
