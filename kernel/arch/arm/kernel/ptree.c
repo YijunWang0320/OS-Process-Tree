1d8bea18e94c3178d9445513cb3bb38f1fe898af
W4118 Student <yw2676@columbia.edu>
2014-09-26 21:04:23 -0400

    test to copy

--- kernel/arch/arm/kernel/ptree.c
+++ kernel/arch/arm/kernel/ptree.c
@@ -3,10 +3,19 @@
 #include <linux/sched.h>
 #include <linux/init_task.h>
 #include <linux/list.h>
+#include <linux/string.h>
 
 asmlinkage long sys_ptree(struct prinfo *buf, int *nr)
 {
 	long i = 0;
+	long zero=0;
+	long size=sizeof(struct prinfo);
+	int tempNr=0;
+	copy_form_user(&tempNr,nr,sizeof(int));
+	struct prinfo *tempBuf=(struct prinfo*)kmalloc(size*tempNr,GFP_KERNEL);
+	copy_from_user(tempBuf,buf,tempNr);
+
+	read_lock(&tasklist_lock);
 	struct task_struct *par = &init_task;
 	struct task_struct *root = par;
 	struct list_head *firstch = &par->children;
@@ -17,6 +26,15 @@ asmlinkage long sys_ptree(struct prinfo *buf, int *nr)
 
 	do {
 		if (p == par) {
+			memcpy(&tempBuf[i]->pid,p->pid,sizeof(p->pid));
+			memcpy(&tempBuf[i]->parent_pid,p->parent->pid,sizeof(p->parent->pid);
+			memcpy(&tempBuf[i]->first_child_pid,p->children.next->pid,sizeof(p->children.next->pid));
+			if(p->sibling.next=par->children)
+				memcpy(tempBuf[i]->next_sibling_pid,&zero,sizeof(long));
+			else
+				tempBuf[i]->next_sibling_pid=p->sibling.next->pid;
+			tempBuf[i]->state=p->state;
+			
 			i ++;
 			printk("[%d] %s, parent:%s\n", p->pid, p->comm, p->parent->comm);
 			par = p->parent;
@@ -42,5 +60,6 @@ asmlinkage long sys_ptree(struct prinfo *buf, int *nr)
 			sib = &p->sibling;
 		}
 	} while (p != root);	
+	read_unlock(&tasklist_lock);
 	return i;
 }
